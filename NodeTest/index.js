cdc305e60e284ae001adf4073a1662bc81765166
JiHwan Park <jihwanpark15@gmail.com>
2023-07-31 14:43:17 +0900

    update NodeTest

---
+++ NodeTest/index.js
@@ -0,0 +1,34 @@
+/*
+var figlet = require("figlet");
+figlet("Take Back", function (err, data) {
+    if (err) {
+        console.log("Something went wrong...");
+        console.dir(err);
+        return;
+    }
+    console.log(data);
+});
+*/
+/*
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res){
+    res.send('Hello World');
+} )
+app.listen(3000)
+*/
+
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.listen(port, ()=> {
+    // Call back 함수
+    setTimeout(()=>{console.log("5초 지남")}, 5000)
+    console.log((`Example app listening on port ${port}`))
+})
+app.get('/', (req, res)=> {
+    res.send('Hello World!')
+})
+
