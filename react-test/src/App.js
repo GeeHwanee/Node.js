832ca292fc286681944123b525eede3a986f9e7b
JiHwan Park <jihwanpark15@gmail.com>
2023-08-01 16:09:31 +0900

    update react-test

---
+++ react-test/src/App.js
@@ -0,0 +1,63 @@
+import logo from './logo.svg';
+import './App.css';
+import {getKeyEventProps} from "@testing-library/user-event/dist/keyboard/getEventProps";
+import {useState} from "react";
+function Header(props){
+    return <header>  
+       <h1><a href="/" onClick={(event)=>{
+           event.preventDefault();
+           props.onChangeMode();
+       }}>{props.title}</a></h1>
+    </header>
+}
+function Nav(props){
+    const lis=[]
+    for (let i=0; i<props.topics.length;i++){
+        let t = props.topics[i];
+        lis.push(<li key={t.id}>
+            <a id={t.id} href={'/read/'+t.id} onClick={(event)=>{
+                event.preventDefault();
+                props.onChangeMode(event.target.id);
+            }}>{t.title}</a>
+        </li>)
+    }
+    return <nav>
+        <ol>
+            {lis}
+        </ol>
+    </nav>
+}
+function Article(props){
+   return <article>
+        <h2>{props.title}</h2>
+       {props.body}
+   </article>
+}
+
+function App() {
+    const [mode, setMode] = useState('WELCOME');
+    const topics = [
+        {id:1,title:'html', body:'html is ...'},
+        {id:2,title:'css', body:'css is ...'},
+        {id:3,title:'js', body:'js is ...'}
+    ]
+    let content = null;
+    if (mode === 'WELCOME'){
+        content =  <Article title="WELCOME" body="Hello! WELCOME"></Article>
+    }else if (mode ==='READ') {
+        content =  <Article title="READ" body="Hello! READ"></Article>
+    }
+  return (
+    <div className="App">
+        <Header title="REACT" onChangeMode={()=>{
+            setMode('WELCOME');
+        }}></Header>
+        <Nav topics={topics} onChangeMode={(id)=>{
+           setMode('READ');
+        }}></Nav>
+        {content}
+    </div>
+  )
+}
+
+export default App;
